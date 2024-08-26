d0ccf43bf4598948020bb3a06a5fc40ea51c8230
Gui Silva <adrianon10@gmail.com>
2024-08-26 03:44:39 -0300

    chore: add @stitches/react

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,5 +1,17 @@
 import { colors } from '@llh-ui/tokens'
+import { styled } from './styles'
+
+const Button = styled('button', {
+  fontFamily: '$default',
+  backgroundColor: '$green-500',
+  borderRadius: '$md',
+})
 
 export const App = () => {
-  return <h1 style={{ color: colors['green-300'] }}>Hello World</h1>
+  return (
+    <>
+      <h1 style={{ color: colors['green-300'] }}>Hello World</h1>
+      <Button>Click</Button>
+    </>
+  )
 }
