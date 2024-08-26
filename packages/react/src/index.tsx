0c680602d32c421de05e665bac3fe7633beeaa74
Gui Silva <adrianon10@gmail.com>
2024-08-26 04:18:12 -0300

    chore: add storybook

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,17 +1,7 @@
-import { colors } from '@llh-ui/tokens'
 import { styled } from './styles'
 
-const Button = styled('button', {
+export const Button = styled('button', {
   fontFamily: '$default',
   backgroundColor: '$green-500',
   borderRadius: '$md',
 })
-
-export const App = () => {
-  return (
-    <>
-      <h1 style={{ color: colors['green-300'] }}>Hello World</h1>
-      <Button>Click</Button>
-    </>
-  )
-}
