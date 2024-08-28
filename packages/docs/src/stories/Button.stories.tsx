b8af6f11852dd2883befb79b215b73519aad59b5
Gui Silva <adrianon10@gmail.com>
2024-08-28 03:24:33 -0300

    docs: add font-sizes, font-weights, font, line-heights, radii and space tokens page

--- packages/docs/src/stories/Button.stories.tsx
+++ packages/docs/src/stories/Button.stories.tsx
@@ -3,7 +3,7 @@ import { Meta, StoryObj } from '@storybook/react'
 import { Button, ButtonProps } from '@llh-ui/react'
 
 export default {
-  title: 'Button',
+  title: 'Form/Button',
   component: Button,
   args: {
     children: 'Enviar'
